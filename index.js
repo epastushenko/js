c0a681f03ae719eaace31d5c7158158df82686e6
ZhQa <epastushenko88@gmail.com>
2024-07-25 22:43:40 +0300

    Homework 1 - Variables, types, operators.

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(function() {
+  mathOperations();
+  typeOperations();
+})();
+
+function mathOperations(num1 = 10, num2 = 4) {
+  console.log("adding =>", num1 + num2);
+  console.log("subtraction =>", num1 + num2);
+  console.log("multiply =>", num1 * num2);
+  console.log("divide =>", num1 / num2);
+  console.log("exponentiation =>",  Math.sqrt(num1, num2));
+  console.log("square root =>", Math.sqrt(num2));
+};
+
+function typeOperations(result) {
+  result = "string";
+  console.log("To string =>", result);
+  result = 2;
+  console.log("To number =>", result);
+  result = !!result;
+  console.log("To boolean =>", result);
+};
