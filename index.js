e65c4d11eea3e71f44eb0de70acffcce63a5173c
ZhQa <epastushenko88@gmail.com>
2024-07-26 13:02:43 +0300

    Homework 1 - Updates.

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 "use strict";
 
-(function() {
-  mathOperations();
-  typeOperations();
-})();
-
 function mathOperations(num1 = 10, num2 = 4) {
   console.log("adding =>", num1 + num2);
-  console.log("subtraction =>", num1 + num2);
+  console.log("subtraction =>", num1 - num2);
   console.log("multiply =>", num1 * num2);
   console.log("divide =>", num1 / num2);
   console.log("exponentiation =>",  Math.sqrt(num1, num2));
@@ -22,3 +17,6 @@ function typeOperations(result) {
   result = !!result;
   console.log("To boolean =>", result);
 };
+
+mathOperations();
+typeOperations();
