a6ced8164efd1d15e3363399fd67eede34e9072c
ZhQa <epastushenko88@gmail.com>
2024-07-30 22:41:32 +0300

    Homework 2 - Add hw 2.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 "use strict";
+
+function lolKek() {
+  for(let i = 1; i < 100; i++) {
+    if(i % 3  === 0 && i % 5 === 0) {
+      console.log(i, 'ЛолКек');
+    } else if(i % 3  === 0 ) {
+      console.log(i, 'Лол');
+    } else if(i % 5  === 0 ) {
+      console.log(i, 'Кек');
+    }
+  }
+};
+
+function evenNumbersWithFor(value = 10) {
+  if(typeof value !== 'number') {
+    console.log('Таке чуство шо Бог десь наказує нас за шось')
+  } else {
+    let result = [];
+    for(let i = 1; i < value; i++) {
+      if(i % 2 === 0) {
+        result.push(i);
+      }
+    }
+    console.log(`${result.join(", ")}.`);
+  }
+}
+
+function evenNumbersWithWhile(value = 10) {
+  if(typeof value !== 'number') {
+    console.log('Таке чуство шо Бог десь наказує нас за шось')
+  } else {
+    let i = 1;
+    let result = [];
+    while(i < value) {
+      if(i % 2 === 0) {
+        result.push(i);
+      }
+      i++;
+    }
+    console.log(`${result.join(", ")}.`);
+  }
+}
+
+lolKek();
+evenNumbersWithFor();
+evenNumbersWithWhile();
