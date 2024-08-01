4567aafe90ae51bd4f18cb158d539d9fd31bc4c0
ZhQa <epastushenko88@gmail.com>
2024-08-01 22:09:58 +0300

    Homework 3 - Add hw 3.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 "use strict";
+
+function recursiveOddSumTo(number) {
+  if(number <= 0) {
+    return 0;
+  } else {
+    return (number % 2 !== 0 ? number : 0) + recursiveOddSumTo(number - 1)
+  }
+};
+  
+console.log(recursiveOddSumTo(1)) // 1
+console.log(recursiveOddSumTo(10)) // 25
+
+function iterativeOddSumTo(number) {
+  let result = 0;
+  for(let i = 0; i <= number; i++) {
+    if(i % 2 !== 0) {
+      result += i;
+    }
+  }
+  return result;
+};
+  
+console.log(iterativeOddSumTo(1)) // 1
+console.log(iterativeOddSumTo(10)) // 25
